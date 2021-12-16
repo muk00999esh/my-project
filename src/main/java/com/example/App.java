fc14e1facfd40fcb84a68c82d2681d2ff1e7a38c
muk00999esh <39761767+muk00999esh@users.noreply.github.com>
2021-12-17 03:31:26 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,6 +9,7 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class App {
 
+    
     public static void main(String[] args)
     {
         SpringApplication.run(App.class, args);
